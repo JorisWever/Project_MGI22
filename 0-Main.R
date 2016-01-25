94620cb6d8910d1098cfad51ae46ac4998612d00
bitterbalpiraat <erwin_vd_berg@hotmail.com>
2016-01-25 14:06:23 +0100

    Commented R main script and created a function to transform .json data into a R dataframe.

--- 0-Main.R
+++ 0-Main.R
@@ -5,41 +5,29 @@ if(!require(rjson)){
 }
 
 # Load Functions
-
+source("r/JsonToDF.R")
 
 
 # Initialize variables
 JSONdataURL <- 'https://gist.githubusercontent.com/paulmillr/4524946/raw/7dc3925fe715f9fbfbc6c4a268e4dbe5f2f3766c/github-users-stats.json'
-out_fn <- 'data/usersR.csv'
+out_fn <- 'data/users_from_R.csv'
 
 
 
 ## 1 - Create a dataframe from JSON data
 
-
-# Create a list from the JSON data
-JSONdata <- fromJSON(file=JSONdataURL)
-
-# Convert the list (and lists inside it) to a data frame
-df <- lapply(JSONdata, function(x)
-{
-  # Convert each group to a data frame.This assumes you have 9 elements each time
-  data.frame(matrix(unlist(x), ncol=9, byrow=T))
-})
-
-# df containts now a list of data frames, connect them together in one single dataframe
-df <- do.call(rbind, df)
-
-
-
-## 2 - Remove rows where the location field is empty
-
+# Call function to transform data 'JSONdataURL' in a dataframe with 9 collumns
+df <- JsonToDF(JSONdataURL, 9)
 
 # Add names to the collumns
 names(df) <- c('name','login','location','language','gravatar','followers','contributions','contributionsStreak','contributionsCurrentStreak')
 
 # Retrieve just the collumns that are needed for further processing
 users_raw <- subset(df, select = c('login','location','language'))
+
+
+
+## 2 - Remove rows where the location field is empty
 
 # Replace empty fields with NA in order to later remove these rows
 users_raw <- sapply(users_raw, as.character)  #This also transform the data into a matrix
@@ -55,4 +43,4 @@ row.names(users) <- 1:nrow(users)
 
 
 ## 3 - Write a csv-file of the cleaned users dataframe for further processing in Python
-write.csv2(users, file = out_fn,row.names = FALSE)
+write.table(users, file = out_fn, row.names = FALSE, col.names = FALSE, sep=';')
